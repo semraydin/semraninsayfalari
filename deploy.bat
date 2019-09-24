c754365677ed8d84ec2326ceed4d4b56de53d1c6
Bora Aydın <boraaaydin@gmail.com>
2019-09-24 14:55:54 +0300

    deploy.bat eklendi

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+docker build . -t dockerdepo.azurecr.io/semraydin/semraninsayfalari:latest
+docker push dockerdepo.azurecr.io/semraydin/semraninsayfalari:latest
+echo kubectl delete replicaset.apps/semraninsayfalari-xxxxxxxxxx  & purge everything from cloudflare
