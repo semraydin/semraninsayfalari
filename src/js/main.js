22460cd8542c8862a265b1f056776d45343bd73c
semraydin <semra.izmit@gmail.com>
2020-01-10 00:27:08 +0300

    semraninsayfaları yenileniyor

--- src/js/main.js
+++ src/js/main.js
@@ -1,284 +1,212 @@
-jQuery(document).ready(function($) {
+(function($) {
+  "use strict";
 
-  'use strict';
+  /*--------------------------
+  preloader
+  ---------------------------- */
+  $(window).on('load', function() {
+    var pre_loader = $('#preloader');
+    pre_loader.fadeOut('slow', function() {
+      $(this).remove();
+    });
+  });
 
-      (function($) {
-        $(".accordion > li:eq(0) a")
-          .addClass("active")
-          .next()
-          .slideDown();
+  /*---------------------
+   TOP Menu Stick
+  --------------------- */
+  var s = $("#sticker");
+  var pos = s.position();
+  $(window).on('scroll', function() {
+    var windowpos = $(window).scrollTop() > 300;
+    if (windowpos > pos.top) {
+      s.addClass("stick");
+    } else {
+      s.removeClass("stick");
+    }
+  });
 
-        $(".accordion a").click(function(j) {
-          var dropDown = $(this)
-            .closest("li")
-            .find("p");
+  /*----------------------------
+   Navbar nav
+  ------------------------------ */
+  var main_menu = $(".main-menu ul.navbar-nav li ");
+  main_menu.on('click', function() {
+    main_menu.removeClass("active");
+    $(this).addClass("active");
+  });
 
-          $(this)
-            .closest(".accordion")
-            .find("p")
-            .not(dropDown)
-            .slideUp();
+  /*----------------------------
+   wow js active
+  ------------------------------ */
+  new WOW().init();
 
-          if ($(this).hasClass("active")) {
-            $(this).removeClass("active");
-          } else {
-            $(this)
-              .closest(".accordion")
-              .find("a.active")
-              .removeClass("active");
-            $(this).addClass("active");
-          }
+  $(".navbar-collapse a:not(.dropdown-toggle)").on('click', function() {
+    $(".navbar-collapse.collapse").removeClass('in');
+  });
 
-          dropDown.stop(false, true).slideToggle();
+  //---------------------------------------------
+  //Nivo slider
+  //---------------------------------------------
+  $('#ensign-nivoslider').nivoSlider({
+    effect: 'random',
+    slices: 15,
+    boxCols: 12,
+    boxRows: 8,
+    animSpeed: 500,
+    pauseTime: 5000,
+    startSlide: 0,
+    directionNav: true,
+    controlNavThumbs: false,
+    pauseOnHover: true,
+    manualAdvance: false,
+  });
 
-          j.preventDefault();
-        });
-      })(jQuery);
+  /*----------------------------
+   Scrollspy js
+  ------------------------------ */
+  var Body = $('body');
+  Body.scrollspy({
+    target: '.navbar-collapse',
+    offset: 80
+  });
 
+  /*---------------------
+    Venobox
+  --------------------- */
+  var veno_box = $('.venobox');
+  veno_box.venobox();
 
+  /*----------------------------
+  Page Scroll
+  ------------------------------ */
+  var page_scroll = $('a.page-scroll');
+  page_scroll.on('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top - 55
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 
-      $('.owl-carousel').owlCarousel({
-          loop:true,
-          margin:30,
-          responsiveClass:true,
-          responsive:{
-              0:{
-                  items:1,
-                  nav:true
-              },
-              500:{
-                  items:1,
-                  nav:false
-              },
-              800:{
-                  items:1,
-                  nav:false
-              },
-              1000:{
-                  items:1,
-                  nav:true,
-                  loop:false
-              },
-              1200:{
-                  items:1,
-                  nav:true,
-                  loop:false
-              },
-              1500:{
-                  items:1,
-                  nav:true,
-                  loop:false
-              }
-          }
-      })
+  /*--------------------------
+    Back to top button
+  ---------------------------- */
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
 
-      
-      $('#form-submit .date').datepicker({
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
+  /*----------------------------
+   Parallax
+  ------------------------------ */
+  var well_lax = $('.wellcome-area');
+  well_lax.parallax("50%", 0.4);
+  var well_text = $('.wellcome-text');
+  well_text.parallax("50%", 0.6);
+
+  /*--------------------------
+   collapse
+  ---------------------------- */
+  var panel_test = $('.panel-heading a');
+  panel_test.on('click', function() {
+    panel_test.removeClass('active');
+    $(this).addClass('active');
+  });
+
+  /*---------------------
+   Testimonial carousel
+  ---------------------*/
+  var test_carousel = $('.testimonial-carousel');
+  test_carousel.owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+  /*----------------------------
+   isotope active
+  ------------------------------ */
+  // portfolio start
+  $(window).on("load", function() {
+    var $container = $('.awesome-project-content');
+    $container.isotope({
+      filter: '*',
+      animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false
+      }
+    });
+    var pro_menu = $('.project-menu li a');
+    pro_menu.on("click", function() {
+      var pro_menu_active = $('.project-menu li a.active');
+      pro_menu_active.removeClass('active');
+      $(this).addClass('active');
+      var selector = $(this).attr('data-filter');
+      $container.isotope({
+        filter: selector,
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
+        }
+      });
+      return false;
+    });
+
+  });
+  //portfolio end
+
+  /*---------------------
+   Circular Bars - Knob
+--------------------- */
+  if (typeof($.fn.knob) != 'undefined') {
+    var knob_tex = $('.knob');
+    knob_tex.each(function() {
+      var $this = $(this),
+        knobVal = $this.attr('data-rel');
+
+      $this.knob({
+        'draw': function() {
+          $(this.i).val(this.cv + '%')
+        }
       });
 
-      /**
-     * jquery.responsive-menu.js
-     * jQuery + CSS Multi Level Responsive Menu
-     */
-
-    jQuery(function($) {
-      $.fn.responsivenav = function(args) {
-        // Default settings
-        var defaults = {
-          responsive: true,
-          width: 993,                           // Responsive width
-          button: $(this).attr('id')+'-button', // Menu button id
-          animation: {                          // Menu animation
-          effect: 'slide',                    // Accepts 'slide' or 'fade'
-          show: 150,
-          hide: 100
-          },
-          selected: 'selected',                 // Selected class
-          arrow: 'downarrow'                    // Dropdown arrow class
-        };
-        var settings = $.extend(defaults, args);
-        
-        // Initialize the menu and the button
-        init($(this).attr('id'), settings.button);
-        
-        function init(menuid, buttonid) {
-          setupMenu(menuid, buttonid);
-          // Add a handler function for the resize and orientationchange event
-          $(window).bind('resize orientationchange', function(){ resizeMenu(menuid, buttonid); });
-          // Trigger initial resize
-          resizeMenu(menuid, buttonid);
-        }
-        
-        function setupMenu(menuid, buttonid) {
-          var $mainmenu = $('#'+menuid+'>ul');
-          
-          var $headers = $mainmenu.find("ul").parent();
-          // Add dropdown arrows
-          $headers.each(function(i) {
-            var $curobj = $(this);
-            $curobj.children('a:eq(0)').append('<span class="'+settings.arrow+'"></span>');
-          });
-          
-          if ( settings.responsive ) {
-            // Menu button click event
-            // Displays top-level menu items
-            $('#'+buttonid).click(function(e) {
-              e.preventDefault();
-              
-              if ( isSelected($('#'+buttonid)) ) {
-                // Close menu
-                collapseChildren('#'+menuid);
-                animateHide($('#'+menuid), $('#'+buttonid));
-              } else {
-                // Open menu
-                animateShow($('#'+menuid), $('#'+buttonid));
-              }
-            });
+      $this.appear(function() {
+        $({
+          value: 0
+        }).animate({
+          value: knobVal
+        }, {
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.val(Math.ceil(this.value)).trigger('change');
           }
-        }
-        
-        function resizeMenu(menuid, buttonid) {
-          var $ww = document.body.clientWidth;
-          
-          // Add mobile class to elements for CSS use
-          // instead of relying on media-query support
-          if ( $ww > settings.width || !settings.responsive) {
-            $('#'+menuid).removeClass('mobile');
-            $('#'+buttonid).removeClass('mobile');
-          } else {
-            $('#'+menuid).addClass('mobile');
-            $('#'+buttonid).addClass('mobile');
-          }
-          
-          var $headers = $('#'+menuid+'>ul').find('ul').parent();
-          
-          $headers.each(function(i) {
-            var $curobj = $(this);
-            var $link = $curobj.children('a:eq(0)');
-            var $subul = $curobj.find('ul:eq(0)');
-            
-            // Unbind events
-            $curobj.unbind('mouseenter mouseleave');
-            $link.unbind('click');
-            animateHide($curobj.children('ul:eq(0)'));
-            
-            if ( $ww > settings.width  || !settings.responsive ) {
-              // Full menu
-              $curobj.hover(function(e) {
-                var $targetul = $(this).children('ul:eq(0)');
-                
-                var $dims = { w: this.offsetWidth,
-                              h: this.offsetHeight,
-                              subulw: $subul.outerWidth(),
-                              subulh: $subul.outerHeight()
-                            };
-                var $istopheader = $curobj.parents('ul').length == 1 ? true : false;
-                $subul.css($istopheader ? {} : { top: 0 });
-                var $offsets = { left: $(this).offset().left, 
-                                 top: $(this).offset().top
-                               };
-                var $menuleft = $istopheader ? 0 : $dims.w;
-                $menuleft = ( $offsets.left + $menuleft + $dims.subulw > $(window).width() ) ? ( $istopheader ? -$dims.subulw + $dims.w : -$dims.w ) : $menuleft;
-                $targetul.css({ left:$menuleft+'px', 
-                               width:$dims.subulw+'px' 
-                              });
-                
-                animateShow($targetul);
-              },
-              function(e) {
-                var $targetul = $(this).children('ul:eq(0)');
-                animateHide($targetul);
-              });
-            } else {
-              // Compact menu
-              $link.click(function(e) {
-                e.preventDefault();
-
-                var $targetul = $curobj.children('ul:eq(0)');
-                if ( isSelected($curobj) ) {
-                  collapseChildren($targetul);
-                  animateHide($targetul);
-                } else {
-                  //collapseSiblings($curobj);
-                  animateShow($targetul);
-                }
-              });
-            }
-          });
-          
-          collapseChildren('#'+menuid);
-          
-          if ( settings.responsive && isSelected($('#'+buttonid)) ) {
-            //collapseChildren('#'+menuid);
-            $('#'+menuid).hide();
-            $('#'+menuid).removeAttr('style');
-            $('#'+buttonid).removeClass(settings.selected);
-          }
-        }
-        
-        function collapseChildren(elementid) {
-          // Closes all submenus of the specified element
-          var $headers = $(elementid).find('ul');
-          $headers.each(function(i) {
-            if ( isSelected($(this).parent()) ) {
-              animateHide($(this));
-            }
-          });
-        }
-        
-        function collapseSiblings(element) {
-          var $siblings = element.siblings('li');
-          $siblings.each(function(i) {
-            collapseChildren($(this));
-          });
-        }
-        
-        function isSelected(element) {
-          return element.hasClass(settings.selected);
-        }
-        
-        function animateShow(menu, button) {
-          if ( !button ) { var button = menu.parent(); }
-          
-          button.addClass(settings.selected);
-          
-          if ( settings.animation.effect == 'fade' ) {
-            menu.fadeIn(settings.animation.show);
-          } else if ( settings.animation.effect == 'slide' ) {
-            menu.slideDown(settings.animation.show);
-          } else {
-            menu.show();
-            menu.removeClass('hide');
-          }
-        }
-        
-        function animateHide(menu, button) {
-          if ( !button ) { var button = menu.parent(); }
-          
-          if ( settings.animation.effect == 'fade' ) {
-            menu.fadeOut(settings.animation.hide, function() { 
-              menu.removeAttr('style');
-              button.removeClass(settings.selected);
-            });
-          } else if ( settings.animation.effect == 'slide' ) {
-            menu.slideUp(settings.animation.hide, function() { 
-              menu.removeAttr('style');
-              button.removeClass(settings.selected);
-            });
-          } else {
-            menu.hide();
-            menu.addClass('hide');
-            menu.removeAttr('style');
-            button.removeClass(settings.selected);
-          }
-        }
-      };
+        });
+      }, {
+        accX: 0,
+        accY: -150
+      });
     });
+  }
 
-    jQuery(function ($) {
-      $('#primary-nav').responsivenav();
-      $('#top-nav').responsivenav({responsive:false});
-    });
-
-
-});
+})(jQuery);
